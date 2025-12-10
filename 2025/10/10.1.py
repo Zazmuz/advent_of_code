a37278d2c62263195c03cc2cbcb6689ab7a93e79
Zazmuz <rasmuswario@gmail.com>
2025-12-10 20:56:09 +0100

    10.1 solved

--- 2025/10/10.1.py
+++ 2025/10/10.1.py
@@ -1,14 +1,15 @@
-with open("sample.txt", "r") as file:
+with open("10.txt", "r") as file:
     lines = file.readlines()
 
+# convert to num
 buttons = []
 for line in lines:
     a, *rest = line.strip().split(" ")
     toggles, joltage = rest[:-1], rest[-1]
 
-    a = a[1:][:-1].replace("#", "1").replace(".", "0")
+    a = a[1:][:-1].replace("#", "0").replace(".", "1")
     size = len(a)
-    a = int(a, 2)
+    a = int(a[::-1], 2)
     flips = []
     for toggle in toggles:
         toggle = toggle[1:][:-1].split(",")
@@ -17,26 +18,23 @@ for line in lines:
             changes |= 1 << (int(bitflipIndex))
         flips.append(changes)
     buttons.append((a, flips, size))
+#
 
-
-def dp(goal, state, used, flipList):
-    print(bin(state), used)
-    if state == 0:
+def dp(goal, state, used, idx, flipList):
+    if state == goal:
         return used
-    if used >= 3:
+    elif idx >= len(flipList):
         return 99999999999999
 
     res = 99999999999999
 
-    for i in range(len(flipList)):
-        newState = state ^ flipList[i]
-        res = min(res, dp(goal, newState, used + 1, flipList))
+    newState = state ^ flipList[idx]
+    res = min(res, dp(goal, state, used, idx+1, flipList), dp(goal, newState, used+1, idx+1, flipList))
     return res
-
 
 s = 0
 for but in buttons:
     a, flips, size = but
     state = (1 << (size)) - 1
-    s = dp(a, state, 0, flips)
-    print(s)
+    s += dp(a, state, 0, 0, flips)
+print(s)
